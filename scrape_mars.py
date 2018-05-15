9b60155044d43f778ff93952ffad09e2c37769c8
misstamu <tamumcbride@yahoo.com>
2018-05-15 15:48:29 -0400

    latest changes

--- scrape_mars.py
+++ scrape_mars.py
@@ -1,147 +1,50 @@
-
-# coding: utf-8
-
-# In[1]:
-
-
-#dependencies
 from splinter import Browser
 from bs4 import BeautifulSoup
-
-
-# In[2]:
+import pandas as pd
+from selenium import webdriver
 
 
 #excute chromedrive for windows
-executable_path = {'executable_path': 'chromedriver.exe'}
-browser = Browser('chrome', **executable_path, headless=False)
+def init_browser():
+    executable_path = {'executable_path': 'chromedriver.exe'}
+    return Browser('chrome', **executable_path, headless=False)
 
 
-# Scraping NASA Mars News
+def scrape():
+    browser = init_browser()
+    mars_data = {}
 
-# In[3]:
-
-
-#url website for scraping
-url = 'https://mars.nasa.gov/news/'
-browser.visit(url)
-
-
-# In[7]:
-
-
-#loop through page to find all titles
-for x in range(50):
+    #url website for scraping
+    url = 'https://mars.nasa.gov/news/'
+    browser.visit(url)
 
     html = browser.html
     soup = BeautifulSoup(html, 'html.parser')
-
     titles = soup.find_all('div', class_='content_title')
-    #t_body = soup.find_all('div', class_='article_teaser_body')
-
-    for title in titles:
-        print(title.text)
-
-
-    browser.click_link_by_partial_text('More')
-
-
-# In[9]:
-
-
-#loop through page to find all title bodies
-for x in range(50):
-
-    html = browser.html
-    soup = BeautifulSoup(html, 'html.parser')
-
     tbody = soup.find_all('div', class_='article_teaser_body')
 
-    for t in tbody:
-        print(t.text)
+    #url website for scraping
+    url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+    browser.visit(url2)
+
+    html = browser.html
+    featured_soup = BeautifulSoup(html, 'html.parser')
+
+    featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/images/mediumsize/PIA16217_ip.jpg'
 
 
-    browser.click_link_by_partial_text('More')
+    mars_weather = 'Sol 2047 (May 10, 2018), Sunny, high 3C/37F, low -71C/-95F, pressure at 7.33 hPa, daylight 05:22-17:20'
 
-
-# JPL Mars Space Featured Image
-
-# In[3]:
-
-
-#url website for scraping
-url2 = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
-browser.visit(url2)
-
-
-# In[12]:
-
-
-html = browser.html
-soup = BeautifulSoup(html, 'html.parser')
-furl = soup.find_all('article', class_='carousel_item')
-print(furl)
-
-
-# In[13]:
-
-
-featured_image_url = 'https://www.jpl.nasa.gov/spaceimages/images/mediumsize/PIA16217_ip.jpg'
-
-
-# Mars Weather
-
-# In[14]:
-
-
-mars_weather = 'Sol 2047 (May 10, 2018), Sunny, high 3C/37F, low -71C/-95F, pressure at 7.33 hPa, daylight 05:22-17:20'
-
-
-# Mars Facts
-
-# In[1]:
-
-
-# panda dependency
-import pandas as pd
-
-
-# In[2]:
-
-
-# url for panda scraping
-url3 = 'https://space-facts.com/mars/'
-
-
-# In[3]:
-
-
-tables = pd.read_html(url3)
-tables
-
-
-# In[7]:
-
-
-mfact_df = tables[0]
-mfact_df.columns = ['Planet Profile', 'Info']
-mfact_df.set_index('Planet Profile', inplace=True)
-mfact_df
-
-
-# In[8]:
-
-
-html_table = mfact_df.to_html
-html_table
-
-
-# Mars Hemispheres
-
-# In[9]:
-
-
-hemisphere_images = [{"title": "Cerberus Hemisphere", 
+    # url for panda scraping
+    url3 = 'https://space-facts.com/mars/'
+    tables = pd.read_html(url3)
+    tables
+    mfact_df = tables[0]
+    mfact_df.columns = ['Planet Profile', 'Info']
+    mfact_df.set_index('Planet Profile', inplace=True)
+    html_table = mfact_df.to_html
+    # Mars Hemispheres
+    hemisphere_images = [{"title": "Cerberus Hemisphere", 
                       "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/cerberus_enhanced.tif/full.jpg"},
     {"title": "Schiaparelli Hemisphere", 
      "img_url": "https://astropedia.astrogeology.usgs.gov/download/Mars/Viking/schiaparelli_enhanced.tif/full.jpg"},
